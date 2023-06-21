ed73c564579cea7b17573c39f172811eb410e129
saidavali22 <93984308+Kumarnadh@users.noreply.github.com>
2023-06-21 09:09:43 +0530

    changed profile pic config

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", async(req,res)=>{
 
 
 // Routes
-app.use("/", UserRoutes)
+app.use("/",  UserRoutes)
 app.use("/", TaskRoutes)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
