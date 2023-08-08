f66f2199cffc2ca068e9851acc69d31e39834d55
saidavali22 <93984308+Kumarnadh@users.noreply.github.com>
2023-08-08 10:36:11 +0530

    Files Upload

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -43,8 +43,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post("/admintask", upload.single('file'), TaskController.assignTask);
-router.post("/admintasktest", upload.array("files", 5), TaskController.assignTaskTest); // Use upload.array() for multiple files
-router.get("/seeAllTask", TaskController.seeAllTask);
+router.post("/admintasktest", upload.array("files", 4), TaskController.assignTaskTest); 
 router.delete("/deleteTask/:id", TaskController.deleteTask);
 router.patch("/editTask/:id", TaskController.editTask);
 
