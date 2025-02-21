65c860abbae9bf8f51fdf8e3870d91f4cfb7697a
Phillip Port <ed1907006@gmail.com>
2025-02-21 19:39:05 +0530

    reminder list based on employee id and list name

--- routes/ReminderRoutes.js
+++ routes/ReminderRoutes.js
@@ -6,7 +6,7 @@ router.post("/reminder", ReminderController.reminder);
 router.get("/reminder", ReminderController.seeAllReminders);
 router.get("/reminder/:id", ReminderController.getSingleReminder);
 router.get("/employeeReminder/:employee_id", ReminderController.getReminderByEmployeeId);
-router.get("/reminderList", ReminderController.getRemindersByList);
+router.post("/reminderList", ReminderController.getRemindersByList);
 router.patch("/reminder/:id", ReminderController.updateReminder);
 router.delete("/reminder/:id", ReminderController.deleteReminder); 
 
